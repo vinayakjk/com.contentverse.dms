71435e810fd90dde3327eae7c3729fd906e4d995
vinayakjk <vinayak.jalanakar@gmail.com>
2024-02-26 16:19:56 +0530

    vinayak login screen code

--- src/main/java/cv_pages/CV_LoginPage.java
+++ src/main/java/cv_pages/CV_LoginPage.java
@@ -1,13 +1,101 @@
 package cv_pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
 
 public class CV_LoginPage {
 
+	WebDriver driver;
+	
+	@CacheLookup	
+	@FindBy(xpath = "//img[@alt='Computhink']")
+	WebElement imgLogo;
+
+	@CacheLookup
+	@FindBy(xpath="//input[@id='userName']")
+	WebElement txtUsername;
+	@CacheLookup
+	@FindBy(xpath="//input[@id='loginPassword']")
+	WebElement txtPassword;
+
+	@FindBy(xpath="//select[@id='rooms']/option[contains(text(),'DMS-SERVER.SUNDYNE')]")
+	WebElement ddlRooms;
+
+	@FindBy(xpath="//button[@id='submitid']")
+	WebElement btnSubmit;
+	
+	@FindBy(xpath="//span[@id='logedinusername']")
+	WebElement imgLoggedInUsername;
+
+	@FindBy(xpath="//label[@for='check1']")
+	WebElement chkRemeberme;
+
+	@FindBy(xpath="//button[@id='forgotPassword']")
+	WebElement btnForgotPassword;
+	
+	@FindBy(xpath="//a[normalize-space()='Logout']")
+	WebElement btnLogout;
+	
+	//Forgot password pop form
+	@FindBy(xpath="//input[@id='userNameforgotpassword']")
+	WebElement txtUserNameForgotPwd;
+
+	@FindBy(xpath="//select[@id='availableRoomsforgotpassword']")
+	WebElement ddlForgotPwdRooms;
+
+	@FindBy(xpath="//button[@id='forgotPasswordsendOTP']")
+	WebElement btnForgotPasswordsendOTP;
+
+	@FindBy(xpath="//span[@id='messageContentForgetPassword']")
+	WebElement msgSendOtpMessge;
+	
+	@FindBy(xpath="//button[@id='messageButtonOKForgetPassword']")
+	WebElement btnMsgOKForgotPassword;
 		
-	@Test
-	public void demo()
+	@FindBy(xpath="//span[@id='forgetPasswordMessageClose']")
+	WebElement btnMsgWindowClose;
+	
+	@FindBy(xpath="//button[@id='forgotpasswordVerifyOtp']")
+	WebElement btnForgotPasswordVerifyOtp;
+	
+	@FindBy(xpath="//input[@id='verifyOTPforgotpassword']")
+	WebElement txtVerifyOTPforgotpassword;
+	
+	
+	public void cv_LoginPage(WebDriver ldriver )
 	{
-		System.out.println("demo");
+		this.driver=ldriver;
+		
+	}
+	
+	
+	public void login_cvWeb(String uid,String pass)
+	{
+		txtUsername.sendKeys(uid);
+		txtPassword.sendKeys(pass);
+		
+		String option="CONTENTVERSE.DEMOROOM";
+
+		//WebElement dropdown =driver.findElement(By.xpath("//select[@id='rooms'][contains(text(), '"+option+"')]"));
+		////select[@id='rooms']/option[contains(text(),'CONTENTVERSE.DEMOROOM')]
+		
+	//	WebElement dropdown =driver.findElement(By.xpath("//select[@id='rooms']/option[contains(text(),'CONTENTVERSE.DEMOROOM')]"));
+		System.out.println(ddlRooms);
+		ddlRooms.click();
+		btnSubmit.click();
+		
+		//check logged in user name shows
+		if (imgLoggedInUsername.isDisplayed()==true)
+		{
+			System.out.println(imgLoggedInUsername.getText() + "logged in succeesfully.");
+			imgLoggedInUsername.click();
+			btnLogout.click();
+			System.out.println("logged out succeesfully.");
+		}
+		
 	}
 }
--- src/test/java/cv_testcases/CV_LoginTestCase.java
+++ src/test/java/cv_testcases/CV_LoginTestCase.java
@@ -1,5 +1,24 @@
 package cv_testcases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import cv_pages.CV_LoginPage;
+import cv_resources.Utility;
+
 public class CV_LoginTestCase {
 
+	
+	@Test
+	public void checkValidUser()
+	{
+		WebDriver driver =Utility.startBrowser("chrome", "http://192.168.1.15:8080/CVWeb/cvLgn");
+		
+		CV_LoginPage login_page= PageFactory.initElements(driver,CV_LoginPage.class);
+		
+		login_page.login_cvWeb("vinayak", "Arnav@123");
+	}
+	
+	
 }
