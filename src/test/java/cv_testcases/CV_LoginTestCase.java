8f818a5ef5cec2b529db40671b0c7beb526408e0
vinayakjk <110488214+vinayakjk@users.noreply.github.com>
2024-02-29 15:06:08 +0530

    Update CV_LoginTestCase.java
    Vinayak:Conflict resolved

--- src/test/java/cv_testcases/CV_LoginTestCase.java
+++ src/test/java/cv_testcases/CV_LoginTestCase.java
@@ -1,41 +1,11 @@
 package cv_testcases;
 
 import org.openqa.selenium.WebDriver;
-<<<<<<< HEAD
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
 import cv_pages.CV_LoginPage;
 import cv_resources.Utility;
-
-//Lgin Page code added - Rajendra 28/02/2024
-
-public class CV_LoginTestCase extends Utility 
-
-{
-	WebDriver gdriver = Utility.startBrowser("chrome", "http://192.168.1.15:8080/CVWeb/cvLgn");
-
-	CV_LoginPage CVL = new CV_LoginPage(gdriver);
-
-	//@BeforeMethod
-	@Test
-	public void lg() {
-
-		CVL.login("rajendra", "pass_123");
-	}
-
-	@AfterMethod
-	
-	public void quit() {
-		wd.quit();
-
-	}
-=======
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
-
-import cv_pages.CV_LoginPage;
-import cv_resources.Utility;
 
 public class CV_LoginTestCase {
 
@@ -50,6 +20,4 @@ public class CV_LoginTestCase {
 		login_page.login_cvWeb("vinayak", "Arnav@123");
 	}
 	
-	
->>>>>>> vinayak-feature
 }
