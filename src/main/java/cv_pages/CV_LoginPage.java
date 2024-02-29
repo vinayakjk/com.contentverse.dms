cf502de7053b7e37e670c8042d80dd67189099de
vinayakjk <110488214+vinayakjk@users.noreply.github.com>
2024-02-29 15:02:35 +0530

    Update CV_LoginPage.java
    conflict resolve manually

--- src/main/java/cv_pages/CV_LoginPage.java
+++ src/main/java/cv_pages/CV_LoginPage.java
@@ -1,19 +1,13 @@
 package cv_pages;
 
-<<<<<<< HEAD
 import java.time.Duration;
-=======
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.Test;
->>>>>>> vinayak-feature
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
@@ -21,47 +15,6 @@ import cv_resources.Utility;
 
 public class CV_LoginPage extends Utility {
 
-	public WebDriver gdriver;
-
-//Webelement - Rajendra Mane 28/02/2024
-	@FindBy(id = "userName")
-	WebElement username;
-
-	@FindBy(id = "loginPassword")
-	WebElement password;
-
-	@FindBy(xpath = "//select[@id='rooms']")
-	WebElement ddproject;
-
-	@FindBy(xpath = "//button[@id='submitid']")
-	WebElement btnlogin;
-
-	@FindBy(xpath = "//div[@id='header-main']")
-	WebElement header;
-
-	@FindBy(xpath = "//span[@id='loginError']")
-	WebElement lbllogin;
-
-	@FindBy(xpath = "//button[@id='cvModelLoginValidationOk']")
-	WebElement sesionmgr;
-
-	@FindBy(xpath = "//div[@class='nameValidation']")
-	WebElement lblusername;
-
-	@FindBy(xpath = "//div[@class='passwordValidation']")
-	WebElement lblpwd;
-
-	@FindBy(xpath = "//span[@id='cvModelLoginValidationMessage']")
-	WebElement validationalert;
-
-//Constructor Rajendra -28/02/2024 
-	public CV_LoginPage(WebDriver gdriver) {
-		this.gdriver = gdriver;
-		gdriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		PageFactory.initElements(gdriver, this);
-
-<<<<<<< HEAD
-=======
 	WebDriver driver;
 	
 	@CacheLookup	
@@ -146,8 +99,7 @@ public class CV_LoginPage extends Utility {
 			btnLogout.click();
 			System.out.println("logged out succeesfully.");
 		}
-		
->>>>>>> vinayak-feature
+
 	}
 
 	public void login(String User, String Pass) 
