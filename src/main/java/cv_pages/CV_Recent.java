f454aab36fd4973268abf08c12a1f5fe49228286
Shra1195 <shradha.patil26@gmail.com>
2024-03-04 18:39:43 +0530

    Shradha_RecentPage_Webelement

--- src/main/java/cv_pages/CV_Recent.java
+++ src/main/java/cv_pages/CV_Recent.java
@@ -1,5 +1,38 @@
 package cv_pages;
 
-public class CV_Recent {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
-}
+import cv_resources.Utility;
+
+public class CV_Recent extends Utility{
+	
+	//RecentTab
+	@FindBy(xpath = "//a[@id='recentMenuBtn']")
+	WebElement  SubMenuRecent;
+	
+	//RecentFolder MouseHovermenu
+	@FindBy(xpath = "//*text() = 'shraddha23')")
+	WebElement SubMenuRecentFolders;
+	
+	//RecentDocument MouseHovermenu
+	@FindBy(xpath = "//*text() = 'Shraddha')")
+	WebElement SubMenuRecentDocuments;
+	
+	
+	
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
