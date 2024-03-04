feb1a8940956886e54bb2be40eab771e1c2a7bce
vinayakjk <110488214+vinayakjk@users.noreply.github.com>
2024-03-04 18:59:50 +0530

    Update CV_Recent.java
    Added comments for code use.

--- src/main/java/cv_pages/CV_Recent.java
+++ src/main/java/cv_pages/CV_Recent.java
@@ -8,17 +8,23 @@ import org.openqa.selenium.support.FindBy;
 import cv_resources.Utility;
 
 public class CV_Recent extends Utility{
-	
+
+	/*
+ 		Elements from this form are dynamic so we need to provide from config or text data .
+   		Text changes as per user login and visited files and folders 
+     		so do changes accordingly with below locators.
+   	
+ 	*/
 	//RecentTab
 	@FindBy(xpath = "//a[@id='recentMenuBtn']")
 	WebElement  SubMenuRecent;
 	
-	//RecentFolder MouseHovermenu
-	@FindBy(xpath = "//*text() = 'shraddha23')")
+	//RecentFolder MouseHovermenu text should be provided by User
+	@FindBy(xpath = "//*text() = 'SCLUpload')")
 	WebElement SubMenuRecentFolders;
 	
-	//RecentDocument MouseHovermenu
-	@FindBy(xpath = "//*text() = 'Shraddha')")
+	//RecentDocument MouseHovermenu  text should be provided by User
+	@FindBy(xpath = "//*text() = 'RecentDoc')")
 	WebElement SubMenuRecentDocuments;
 	
 	
